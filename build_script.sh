5d8210de186bd32428fcb6b0319edb45e56c9780
naj <wizebg403@gmail.com>
2024-07-28 00:13:32 +0800

    Delete build_script.sh

--- build_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install Playwright and browsers
-pip install playwright
-playwright install chromium
-mkdir -p playwright-browsers
-mv /home/vcap/app/.cache/ms-playwright playwright-browsers/
