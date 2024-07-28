092ed65d6012657bb7dc0c3951262dc052e9531b
naj <wizebg403@gmail.com>
2024-07-28 17:03:15 +0800

    Delete api directory

--- api/tts.py
+++
@@ -1,8 +0,0 @@
-from http.server import BaseHTTPRequestHandler
-from urllib.parse import parse_qs
-
-def handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': 'Hello from the TTS API!'
-    }
